6b71234c072cf5d0998b3909fabfee15ad5cac8d
umarfaruq273 <umarfaruq273@gmail.com>
2026-01-18 22:49:38 +0800

    This is my first commit to Github!

---
+++ Day001 - Absolute Basics - print(), variables, comments.py
@@ -0,0 +1,13 @@
+
+#Print function
+print("<><><><><>><><><><><><><><><>")
+print("Hello World")
+print("This is the 1st try of Python commit to Github")
+print("<><><><><>><><><><><><><><><>")
+
+#Variables
+x="I will commit everyday"
+y="<><><><><>><><><><><><><><><>"
+print(x)
+print(y)
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python-100-days
 Python fundamentals + discipline
+
+Practice how print() function works, variables and comments
