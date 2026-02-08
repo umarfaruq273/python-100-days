f0e1865dba797626ce0abdff5acdaac3f46349f8
umarfaruq273 <umarfaruq273@gmail.com>
2026-02-08 23:25:12 +0800

    import statistics and import sys

--- Day003 - String and Bytes Literals.py
+++ Day003 - String and Bytes Literals.py
@@ -4,11 +4,27 @@ y = 'eggs'
 print(x, y)
 print(x+y)
 print("x " + 'y')
+
+#Another way of using quote in strings
 print('1 - Say "Hello", please.'"Don't do that!")
 print('2 - Say "Hello", please.',"Don't do that!")
 print('3 - Say "Hello", please.', "Don't do that!")
 print('4 - Say "Hello", please.'
       "Don't do that!")
+print("5 - Say \"Hello\" to everyone!")
+
+#Triple-quoted strings
+print("""6 - This is a triple-quoted string.""")
+print("""7 - This string has "quotes" inside.""")
+print('''8 - This triple-quoted string
+continues on the next line.''')
+
+#Using backslash in strings by ignoring newline
+print('This string will not include \
+backslashes or newline characters.')
+
+#backlslash escape a backslash
+print('C:\\Program Files')
 
 #bytes literal
 print(b'\x89PNG\r\n\x1a\n')
